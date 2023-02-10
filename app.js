4cd5fa5cc8a9f2af3b8bfde88e443a7c715c5e8d
Josephzal <lombardojd94@gmail.com>
2023-02-10 15:50:23 -0800

    Add Authentication via Passport

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 const { emitWarning } = require('process');
 
 mongoose.set('strictQuery', false);
@@ -35,6 +38,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set up cookies
 const sessionConfig = {
     secret: 'thisisasecret',
     resave: false,
@@ -49,7 +53,17 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+// Authentication via passport
+app.use(passport.initialize());
+app.use(passport.session());
+// Tell passport to use local strategy and for the local strategy, use authentication method on user model
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
@@ -60,8 +74,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
@@ -72,7 +87,7 @@ app.use((err, req, res, next) => {
     if(err){
         req.flash('error', "Page Not Found");
         return res.redirect(`/campgrounds`);
-    }
+    };
     if(!err.message) err.message = "Oh No! Something went wrong."
     res.status(statusCode).render('error', { err });
 });
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const {campgroundSchema} = require('../schemas.js');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { request } = require('express');
+const {isLoggedIn} = require('../middleware');
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -21,11 +21,11 @@ router.get('/', catchAsync(async(req, res) => {
     res.render('campgrounds/index', {campgrounds});
 }));
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Config')
     const campground = new Campground(req.body.campground);
     await campground.save();
@@ -42,7 +42,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', {campground});
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     if(!campground){
         req.flash('error', 'Campground Not Found!');
@@ -51,13 +51,13 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', {campground});
 }));
 
-router.put('/:id', validateCampground, catchAsync(async (req, res, next) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Campground Deleted!');
